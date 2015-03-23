8b0971d64cad56adb2af6c03e7e4e3fec75e1360
axton21 <axton_n@yahoo.com>
2015-03-23 18:33:23 -0400

    done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,37 @@
-# code the ProductsController here
-# it should inherit from ApplicationController
+class ProductsController < ApplicationController
+
+	get '/products' do
+		@products = Product.all
+		erb :'products/index'
+	end
+
+	get '/products/new' do
+		erb :'products/new'
+	end
+
+	get '/products/:id' do
+		@product = Product.find(params["id"])
+		erb :'products/show'
+	end
+
+	get '/products/:id/edit' do
+		@product = Product.find(params["id"])
+		erb :'/products/edit'
+	end
+
+	post '/products' do
+		@product = Product.create(params["product"])
+		redirect "/products/#{@product.id}"
+	end
+
+	patch '/products/:id' do
+		@product = Product.find(params[:id])
+		@product.update(params["product"])
+		redirect "/products/#{@product.id}"
+	end
+
+	delete '/products/:id' do
+		Product.find(params["id"]).destroy
+		redirect "/products"
+	end
+end
